98633b255fbbab49e0944ceaf06aea9a119d3548
Sivabaalan.BA <vbmsbaalan@gmail.com>
2026-09-08 14:15:13 +0530

    feat: Enhance responsiveness and layout adjustments across components and views

--- webapp/Component.js
+++ webapp/Component.js
@@ -2,8 +2,9 @@ sap.ui.define([
     "sap/ui/core/UIComponent",
     "xyraweb/model/models",
     "xyraweb/model/GlobalLoading",
-    "xyraweb/model/NotificationService"
-], (UIComponent, models, GlobalLoading, NotificationService) => {
+    "xyraweb/model/NotificationService",
+    "sap/ui/core/Popup"
+], (UIComponent, models, GlobalLoading, NotificationService, Popup) => {
     "use strict";
 
     return UIComponent.extend("xyraweb.Component", {
@@ -18,6 +19,42 @@ sap.ui.define([
             // call the base component's init function
             UIComponent.prototype.init.apply(this, arguments);
 
+            // Install automatic coordinate compensator for SAPUI5 Popups when CSS zoom is active
+            if (Popup && !Popup._xyraZoomPatched) {
+                Popup._xyraZoomPatched = true;
+                const fnOrigApplyPosition = Popup.prototype._applyPosition;
+                Popup.prototype._applyPosition = function(oPosition) {
+                    fnOrigApplyPosition.apply(this, arguments);
+
+                    const z = parseFloat(window.getComputedStyle(document.documentElement).zoom) || 1;
+                    if (Math.abs(z - 1) > 0.005 && this._$) {
+                        const $Ref = this._$();
+                        if ($Ref && $Ref.length && $Ref[0]) {
+                            const dom = $Ref[0];
+                            // Do not adjust Dialogs or Message Boxes which are centered via CSS fixed positioning
+                            if (dom.classList.contains("sapMDialog") || dom.classList.contains("sapMMessageBox") || dom.classList.contains("sapMMessageDialog")) {
+                                return;
+                            }
+                            if (dom.style.left && dom.style.left.indexOf("px") > -1) {
+                                dom.style.left = (parseFloat(dom.style.left) / z) + "px";
+                            }
+                            if (dom.style.right && dom.style.right.indexOf("px") > -1) {
+                                dom.style.right = (parseFloat(dom.style.right) / z) + "px";
+                            }
+                            if (dom.style.top && dom.style.top.indexOf("px") > -1) {
+                                dom.style.top = (parseFloat(dom.style.top) / z) + "px";
+                            }
+                            if (dom.style.width && dom.style.width.indexOf("px") > -1) {
+                                dom.style.width = (parseFloat(dom.style.width) / z) + "px";
+                            }
+                            if (dom.style.minWidth && dom.style.minWidth.indexOf("px") > -1) {
+                                dom.style.minWidth = (parseFloat(dom.style.minWidth) / z) + "px";
+                            }
+                        }
+                    }
+                };
+            }
+
             // set device model & global notifications model
             this.setModel(models.createDeviceModel(), "device");
             this.setModel(NotificationService.getModel(), "notifications");
@@ -29,8 +66,18 @@ sap.ui.define([
             if (oRouter) {
                 oRouter.attachBeforeRouteMatched((oEvent) => {
                     var sRouteName = oEvent.getParameter("name");
+                    var docEl = document.documentElement;
                     if (sRouteName === "Login") {
                         mVisitedRoutes = {};
+                        if (docEl) {
+                            docEl.classList.remove("xyra-app-zoomed");
+                            docEl.classList.add("xyra-login-active");
+                        }
+                    } else {
+                        if (docEl) {
+                            docEl.classList.add("xyra-app-zoomed");
+                            docEl.classList.remove("xyra-login-active");
+                        }
                     }
                     if (GlobalLoading.isAllowedRoute(sRouteName)) {
                         if (bInitialPageLoad || !mVisitedRoutes[sRouteName]) {
